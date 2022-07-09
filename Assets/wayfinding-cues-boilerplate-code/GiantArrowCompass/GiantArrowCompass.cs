d3eb5243766566e78bb212e544c2a2ef38cfd8d7
SaitenHazard <khantowsifkhan@gmail.com>
2022-07-09 12:07:36 +0200

    done

--- Assets/wayfinding-cues-boilerplate-code/GiantArrowCompass/GiantArrowCompass.cs
+++ Assets/wayfinding-cues-boilerplate-code/GiantArrowCompass/GiantArrowCompass.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class GiantArrowCompass : MonoBehaviour
 {
-    [SerializeField] private Transform target;
+    //[SerializeField] private Transform target;
 
+    public Transform target;
+
+    private void Start()
+    {
+        target = Target.Instance.transform;
+    }
     private void Update()
     {
         transform.LookAt(target.position);
